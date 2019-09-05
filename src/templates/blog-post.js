c83730725d6b01a1a281ce608016577fbc517f90
Christopher <chris132128@gmail.com>
2019-09-05 02:07:33 -0700

    add previous and next button to each post

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -3,6 +3,8 @@ import { Link } from 'gatsby'
 import IconButton from '@material-ui/core/IconButton'
 import { ArrowBack } from '@material-ui/icons'
 import moment from 'moment'
+import Button from '@material-ui/core/Button'
+import ButtonGroup from '@material-ui/core/ButtonGroup'
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -10,10 +12,13 @@ import SEO from "../components/seo"
 export default function Template({ data }) {
   const post = data.markdownRemark
   const date = moment(post.frontmatter.date).format('MMMM Do, YYYY')
+  const { title, path, } = post.frontmatter
+  const { html, } = post
+  const pathNum = path.slice(1, path.length) * 1
 
   return(
     <Layout>
-      <SEO title={ post.frontmatter.title } />
+      <SEO title={ title } />
       <IconButton
         aria-label="back"
         component={ Link }
@@ -27,11 +32,38 @@ export default function Template({ data }) {
       </IconButton>
       {/* <Link to="blog">Go Back</Link> */}
       <hr />
-      <h1>{ post.frontmatter.title }</h1>
+      <h1>{ title }</h1>
+      <div
+        style={{
+          paddingBottom: '20px',
+          listStyle: 'none',
+          justifyContent: 'space-evenly',
+        }}
+      >
+        <ButtonGroup>
+          {
+            pathNum > 1 && 
+            <Button
+              aria-label="previous"
+              component={ Link }
+              to={ `${pathNum - 1}` }
+            >
+              Previous
+            </Button>
+          }
+          <Button
+            aria-label="next"
+            component={ Link }
+            to={ `${pathNum + 1}` }
+          >
+            Next
+          </Button>
+        </ButtonGroup>
+      </div>
       <h4 style={{
         color: 'grey',
       }}>{ date }</h4>
-      <div dangerouslySetInnerHTML={{ __html: post.html }} />
+      <div dangerouslySetInnerHTML={{ __html: html }} />
     </Layout>
   )
 }
